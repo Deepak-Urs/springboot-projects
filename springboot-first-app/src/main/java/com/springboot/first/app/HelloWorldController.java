7c681efe4eb49c6f6dfa8a3246a105e1bd0337f6
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-28 20:15:02 -0800

    /hello-world GET API

---
+++ springboot-first-app/src/main/java/com/springboot/first/app/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.springboot.first.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+//@ResponseBody
+@RestController
+public class HelloWorldController {
+
+//	To make a java class a controller, we use @Controller
+//	POSSIBLE REST APIS that could be written
+//	get
+//	post
+//	put
+//	delete
+//	returns JSON/XML or other representational form to the client
+//	To do the above , we use @ResponseBody
+//  //@Controller + @ResponseBody are subset of @RestController 
+	
+//	To serve a method as a GET - REST API end point, we use @GetMapping
+//  If a a client uses http://localhost:8080/hello-world
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World! from first REST API";
+	}
+}
