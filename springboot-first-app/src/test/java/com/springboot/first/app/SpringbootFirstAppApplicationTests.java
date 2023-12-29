d3baf54d050b936e479ec82356f9e353a98cc97b
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-28 19:57:07 -0800

    springboot first app

---
+++ springboot-first-app/src/test/java/com/springboot/first/app/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.first.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstAppApplicationTests {
+	
+//	Here we write all the JUnit test cases
+	@Test
+	void contextLoads() {
+	}
+
+}
