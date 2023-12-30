86a09f5895fdfdebae0d35122271e1a66cdafc60
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-29 16:43:12 -0800

    SB RestAPI returning a List

--- springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
+++ springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
@@ -1,5 +1,8 @@
 package com.springboot.first.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 	
 	//	http://localhost:8080/student
-	// @GetMapping(path = "/student", produces=MediaType.APPLICATION_XML_VALUE)	-> to get XML o/p 
 	@GetMapping("/student")
 	public Student getStudent() {
 //		SB by default uses Jackson2 library to convert JAVA object to JSON and JSON to JAVA object
 		return new Student("Deepak", "Urs");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+//		SB by default uses Jackson2 library to convert JAVA object to JSON and JSON to JAVA object
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Deepak", "Urs"));
+		students.add(new Student("A", "B"));
+		students.add(new Student("C", "D"));
+		students.add(new Student("E", "F"));
+		students.add(new Student("G", "H"));
+		return students;
 	}
 }
