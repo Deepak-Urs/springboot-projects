5c8599b10f5f92567ece0fedb09114e4f86b5d04
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-02 14:36:08 -0800

    Step6: Starting to create Employee REST API

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import net.javaguides.springboot.service.EmployeeService;
+
+@Service
+//@Transactional --> not needed as the Spring JPA internally gives transcation operations to its methods
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	private EmployeeRepository employeeRepository;
+	
+//	@Autowired -> not needed now
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+	
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return employeeRepository.save(employee);
+	}
+	
+}
