04e85ae25f521fe586ccd842e7996a00d3bfc4a0
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-02 14:34:34 -0800

    auto import for the JpaRepository

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package net.javaguides.springboot.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import net.javaguides.springboot.model.Employee;
+
+
+//@Repository -> not needed as JpaRepository<> itself gives the recognition
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
 }
