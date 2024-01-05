221527686c1c8eb6ecce18d78c2e7c8c44ae866f
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-04 18:41:56 -0800

    Step-10: Built the deleteEmployee(DELETE) API call

--- springboot-backend/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -63,6 +63,13 @@ public class EmployeeServiceImpl implements EmployeeService{
 		
 		return existingEmployee;
 	}
+
+	@Override
+	public void deleteEmployee(long id) {
+		employeeRepository.findById(id).orElseThrow(
+								() -> new ResourceNotFoundException("Employee", "Id", id));
+		employeeRepository.deleteById(id);
+	}
 	
 	
 }
