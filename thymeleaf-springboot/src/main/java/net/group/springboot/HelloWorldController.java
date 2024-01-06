4b94a68f98d812eb4294d795c478b3d810bcb13c
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-05 16:25:03 -0800

    ThymeLeaf Springboot app

---
+++ thymeleaf-springboot/src/main/java/net/group/springboot/HelloWorldController.java
@@ -0,0 +1,16 @@
+package net.group.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloWorldController {
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello World! from Thymeleaf-Springboot");
+		return "Hello World! from Thymeleaf-Springboot";
+	}
+}
---
+++ thymeleaf-springboot/src/test/java/net/group/springboot/ThymeleafSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.group.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
