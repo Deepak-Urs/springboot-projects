db3cda2c100f575571024b273bfce60bfb3fa870
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-12 14:54:43 -0800

    Implemented Delete Student by Id feature, completion of Student Management System App

--- student-management-system/src/main/java/net/group/sms/service/impl/StudentServiceImpl.java
+++ student-management-system/src/main/java/net/group/sms/service/impl/StudentServiceImpl.java
@@ -44,4 +44,11 @@ public class StudentServiceImpl implements StudentService{
 		return studentRepository.save(student);
 	}
 
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);
+		
+	}
+
 }
