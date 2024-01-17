e38a6c6bd174a9e5ef38fb2d3e2c11a8e12e958c
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-17 14:42:33 -0800

    Quiz App: Starting microservices based application

---
+++ microservices/question-service/src/test/java/com/group/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
