53128aca6a2e4d68d47ebc1984f7f2493db0b3ba
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-21 15:09:04 -0800

    Implemented getQuizQuestions which includes a wrapper

--- microservices/question-service/src/main/java/com/group/questionservice/controller/QuizController.java
+++ microservices/question-service/src/main/java/com/group/questionservice/controller/QuizController.java
@@ -1,13 +1,19 @@
 package com.group.questionservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//import com.group.questionservice.model.Question;
+import com.group.questionservice.model.QuestionWrapper;
 import com.group.questionservice.service.QuizService;
 
 @RestController
@@ -20,5 +26,11 @@ public class QuizController {
 	@PostMapping("create")
 	public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam Integer numQ, @RequestParam String title) {
 		return quizService.createQuiz(category, numQ, title);
+	}
+	
+	@GetMapping("get/{id}")
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+//		wrapper for getting all data without answers	
+		return quizService.getQuizQuestions(id);
 	}
 }
--- microservices/question-service/src/main/java/com/group/questionservice/service/QuizService.java
+++ microservices/question-service/src/main/java/com/group/questionservice/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.group.questionservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.group.questionservice.dao.QuestionDao;
 import com.group.questionservice.dao.QuizDao;
 import com.group.questionservice.model.Question;
+import com.group.questionservice.model.QuestionWrapper;
 import com.group.questionservice.model.Quiz;
 
 @Service
@@ -31,6 +34,20 @@ public class QuizService {
 		quizDao.save(quiz);
 		
 		return new ResponseEntity<>("Success", HttpStatus.CREATED);
+		
+	}
+
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+		Optional<Quiz> quiz = quizDao.findById(id);
+		List<Question> questionsFromDB = quiz.get().getQuestions();
+		List<QuestionWrapper> questionsForUser = new ArrayList<>();
+		
+		for(Question q: questionsFromDB) {
+			QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestion(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+			questionsForUser.add(qw);
+		}
+		
+		return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 		
 	}
 	
