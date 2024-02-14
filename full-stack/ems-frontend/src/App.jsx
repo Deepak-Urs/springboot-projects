e25f136cb90d4fb1979c38b8c15f8cfd2bc7cc6f
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-13 16:57:23 -0800

    added Header and Footer components

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import HeaderComponent from './Components/HeaderComponent'
 import ListEmployeeComponents from './Components/ListEmployeeComponents'
+import FooterComponent from './Components/FooterComponent'
 
 function App() {
 
   return (
+    <>
+    <HeaderComponent />
     <ListEmployeeComponents />
+    <FooterComponent />
+    </>
   )
 }
 
---
+++ full-stack/ems-frontend/src/Components/HeaderComponent.jsx
@@ -0,0 +1,11 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header className="navbar navbar-dark bg-dark">
+        <h1 className="navbar-brand" href="">Employee Management System</h1>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
