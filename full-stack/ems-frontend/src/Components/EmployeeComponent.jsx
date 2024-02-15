aacec1af2ec03b609e88803084983eb942cbe7e1
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-14 16:22:24 -0800

    Connecting App to the 'Add-Employee' REST endpoint

--- full-stack/ems-frontend/src/Components/EmployeeComponent.jsx
+++ full-stack/ems-frontend/src/Components/EmployeeComponent.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react"
+import { createEmployee } from "../services/EmployeeService"
+import { useNavigate } from "react-router-dom"
 
 const EmployeeComponent = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const navigator = useNavigate();
 
   const saveEmployee = (e) => {
     e.preventDefault();
 
     const employee = {firstName, lastName, email}
     console.log(employee);
+
+    createEmployee(employee).then(res => {
+      console.log(res.data);
+      navigator('/employees')
+    }).catch(err => {
+      console.log(err);
+    })
   }
 
   return (
