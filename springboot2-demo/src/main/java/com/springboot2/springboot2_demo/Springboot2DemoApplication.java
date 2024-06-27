e863fee90534b60ca7c5466f641df824b0a0c4cc
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-06-26 17:26:38 -0700

    Spring-boot[SB]: Intro setup and auto config

---
+++ springboot2-demo/src/main/java/com/springboot2/springboot2_demo/Springboot2DemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboot2.springboot2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Spring Boot!");
+		SpringApplication.run(Springboot2DemoApplication.class, args);
+	}
+
+}
