7d3779353eea964d55feb364b1a3814a0c310603
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-06-26 17:25:58 -0700

    updating missed out codes

---
+++ Fundamentals/src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+	public static void main(String args[]) {
+//		 this is a comment line
+		/* this is a 
+		 * multicomment
+		 * line
+		 */
+		int a = -10;
+		System.out.println(a);
+	
+		
+	}
+}
---
+++ Fundamentals/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author deepakurs
+ *
+ */
+module Fundamentals {
+}
